abb8ab8ab65fafc5140c7fa475e71bf227005475
exusiai <1617218599@qq.com>
2020-09-18 21:54:22 +0800

    the changes on 9.18

---
+++ compareStr.java
@@ -0,0 +1,16 @@
+public class compareStr{
+    public static void main(String args[]){
+        int a=10;
+        int b=10;
+        System.out.println(a==b);//true
+        String str1=new String("abc");
+        String str2=new String("abc");
+        System.out.println(str1.equals(str2));//true
+        System.out.println(str1==str2);//false
+        //this compares the pointer of two srings,to judge whether they point the same object
+        String str3="abc";
+        String str4="abc";
+        System.out.println(str3==str4);//true here, the two string shares the same memory
+        System.out.println(str3.equals(str4));//true
+    }
+}
--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
 
     public static void main(String[] args){
         System.out.println("helloworld\n");
+        //int[] score=new int[3]{89,79,76};不能在指定内容的同时指定数组长度
     }
 }
