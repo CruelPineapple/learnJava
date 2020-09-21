72698cd51000f9216c9560b84458dfa615f5b35a
exusiai <1617218599@qq.com>
2020-09-21 20:23:20 +0800

    foud a way to encode chinese words

--- compareStr.java
+++ compareStr.java
@@ -12,5 +12,6 @@ public class compareStr{
         String str4="abc";
         System.out.println(str3==str4);//true here, the two string shares the same memory
         System.out.println(str3.equals(str4));//true
+        System.out.println("中文");//编译存在中文的程序时，使用javac -encoding UTF-8 fileName.java
     }
 }
