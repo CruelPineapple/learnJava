fbab9f4d3398321466a78edcb64cfc3c0bd693af
exusiai <1617218599@qq.com>
2020-10-08 16:01:54 +0800

    finish peach

---
+++ 10-homework/src/peach/peach.java
@@ -0,0 +1,11 @@
+package peach;
+
+public class peach {
+    public static void main(String [] args){
+        int peach=1;
+        for (int day=1;day<10;day++){
+            peach=(peach+1)*2;
+        }
+        System.out.println("第一天摘了"+peach+"个桃子");
+    }
+}
