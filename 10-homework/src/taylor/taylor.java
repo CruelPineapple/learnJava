96b99c2eac167989b7fb83cf70096335ef237c3d
exusiai <1617218599@qq.com>
2020-10-10 22:22:38 +0800

    finish taylor

---
+++ 10-homework/src/taylor/taylor.java
@@ -0,0 +1,20 @@
+package taylor;
+
+import java.util.Scanner;
+
+public class taylor {
+    public static void main(String [] args){
+        Scanner scan=new Scanner(System.in);
+        double x=Double.parseDouble(scan.next());
+        int i;
+        int n=8;
+        double cos=1,t=1;
+        for (i=1;i<=n;i++){
+            t *=x*x;
+            t /=2*i*(2*i-1);
+            t *= -1;
+            cos +=t;
+        }
+        System.out.println("cos("+x+")="+cos);
+    }
+}
