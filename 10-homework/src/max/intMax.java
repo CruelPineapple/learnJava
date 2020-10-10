9f7e97dd65a2073396da5582370ac1159033aade
exusiai <1617218599@qq.com>
2020-10-10 17:25:10 +0800

    finish int max()

---
+++ 10-homework/src/max/intMax.java
@@ -0,0 +1,18 @@
+package max;
+
+public class intMax {
+    private static int max(int a,int b,int c){
+        if (a>b&&a>c){
+            return a;
+        }else if (b>a&&b>c){
+            return b;
+        }else if (c>a&&c>b){
+            return c;
+        }else {
+            return -1;
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(max(7,8,9));
+    }
+}
